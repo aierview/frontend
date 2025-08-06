10e4936441eb2651035e4e1cbc781594d191e0aa
gervasioartur <gervasioartur@outlook.com.br>
2025-08-06 20:43:21 -0300

    test: ensure AuthRepository throws UnexpectedError when post /auth/local/signup retruns ServerError

--- tests/infra/adapter/AuthRepository.test.ts
+++ tests/infra/adapter/AuthRepository.test.ts
@@ -1,3 +1,5 @@
+import { HttpStatusCode } from "@/domain/enums/HttpStatusCode";
+import { UnexpectedError } from "@/domain/errors/UnexpectedError";
 import { LocalSignupRequest } from "@/domain/model/LocalSignupRequest";
 import { AuthRepository } from "@/infra/adapter/AuthRepository";
 import { anyLocalSignupRequest } from "@@/shared/testdata/auth-test-fixture";
@@ -7,15 +9,16 @@ const mockAxiosClient = {
   post: vi.fn(),
 };
 
-const makeSut = () => {
-  const sut = new AuthRepository(mockAxiosClient as any);
-  return { sut };
-};
-
 describe("AuthRepository", () => {
-  it("should call post auth/local/signup with correct parameters", async () => {
+  let sut: AuthRepository;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    sut = new AuthRepository(mockAxiosClient as any);
+  });
+
+  it("should call post auth/local/signup with correct parameters on localSignup", async () => {
     // Arrangetest
-    const { sut } = makeSut();
     mockAxiosClient.post.mockResolvedValueOnce({ statusCode: 201 });
 
     const request: LocalSignupRequest = anyLocalSignupRequest();
@@ -27,5 +30,17 @@ describe("AuthRepository", () => {
       url: "/auth/local/signup",
       body: request,
     });
+  });
+
+  it("should thow UnexpectedError on localSignup", async () => {
+    // Arrangetest
+    mockAxiosClient.post.mockResolvedValueOnce({
+      statusCode: HttpStatusCode.serverError,
+      body: "dados inválidos",
+    });
+    const request: LocalSignupRequest = anyLocalSignupRequest();
+
+    // Assert
+    await expect(sut.localSignup(request)).rejects.toThrow(UnexpectedError);
   });
 });
