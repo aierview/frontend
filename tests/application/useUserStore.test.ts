3514844e1406a54ca5d19acc7e69280ae6623767
gervasioartur <gervasioartur@outlook.com.br>
2025-08-06 08:51:47 -0300

    feat: ensure useAuthStore calls signup with correct params and hanldes error on localSignup

---
+++ tests/application/useUserStore.test.ts
@@ -0,0 +1,72 @@
+import { createAuthStore } from "@/application/store/useAuthStore";
+import { UserRole } from "@/domain/enums/UserRole";
+import { LocalSignupRequest } from "@/domain/model/LocalSignupRequest";
+import { AuthRepository } from "@/infra/adapter/AuthRepository";
+import { AxiosError } from "axios";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+describe("useAuthStore", () => {
+  let store: ReturnType<ReturnType<typeof createAuthStore>["getState"]>;
+  let mockRepo: { localSignup: ReturnType<typeof vi.fn> };
+  let zustandStore: ReturnType<typeof createAuthStore>;
+
+  beforeEach(() => {
+    // Criamos um mock do repositório
+    mockRepo = {
+      localSignup: vi.fn(),
+    };
+
+    // Criamos a store injetando o mock
+    zustandStore = createAuthStore(mockRepo as unknown as AuthRepository);
+    store = zustandStore.getState();
+  });
+
+  it("should call localSignup with correct parameters", async () => {
+    const request: LocalSignupRequest = {
+      email: "gervasio@example.com",
+      password: "senhaSegura123",
+      name: "Gervasio",
+      role: UserRole.FULLSTACK,
+    };
+
+    // Simula sucesso da API
+    mockRepo.localSignup.mockResolvedValueOnce(undefined);
+
+    await zustandStore.getState().localSignup(request);
+
+    expect(mockRepo.localSignup).toHaveBeenCalledWith(request);
+    expect(zustandStore.getState().isLoading).toBe(false);
+    expect(zustandStore.getState().error).toBe(null);
+  });
+
+  it("should handle error on localSignup", async () => {
+    const request: LocalSignupRequest = {
+      email: "erro@example.com",
+      password: "senha123",
+      name: "Erro",
+      role: UserRole.FULLSTACK,
+    };
+
+    // Simula erro da API (AxiosError-like)
+    const fakeError = new AxiosError(
+      "Request failed",
+      "400",
+      {},
+      {},
+      {
+        data: { data: "Email already in use!" },
+        status: 400,
+        statusText: "Bad Request",
+        headers: {},
+        config: {},
+      }
+    );
+    mockRepo.localSignup.mockRejectedValueOnce(fakeError);
+
+    await zustandStore.getState().localSignup(request);
+
+    expect(mockRepo.localSignup).toHaveBeenCalledWith(request);
+    expect(zustandStore.getState().isLoading).toBe(false);
+    expect(zustandStore.getState().error).toBe("Email already in use!");
+  });
+});
