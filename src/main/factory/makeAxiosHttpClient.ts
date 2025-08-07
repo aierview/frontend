13fd012cd50f2eb2d9d65027a2592e0dd4188007
gervasioartur <gervasioartur@outlook.com.br>
2025-08-07 06:08:07 -0300

    fix: adding external url to axios

--- src/main/factory/makeAxiosHttpClient.ts
+++ src/main/factory/makeAxiosHttpClient.ts
@@ -1,6 +1,9 @@
 import { AxiosHttpClient } from "@/infra/axios/AxiosHttpClient";
 import AxiosHttpClientInstance from "@/shared/utils/AxiosHttpClientInstance";
+import { NEXT_PUBLIC_API_URL } from "@/shared/utils/lib";
 
 export const makeAxiosHttpClient = (): AxiosHttpClient => {
-  return new AxiosHttpClient(AxiosHttpClientInstance.getInstance());
+  return new AxiosHttpClient(
+    AxiosHttpClientInstance.getInstance(NEXT_PUBLIC_API_URL)
+  );
 };
